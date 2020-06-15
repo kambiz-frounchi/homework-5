11be0fd9915f562b0d0433255d0d7228cabd4f6f
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-06-15 01:05:52 -0400

    work day scheduler

---
+++ script.js
@@ -0,0 +1,52 @@
+var currentDay = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").text(currentDay);
+var currentHour = moment().get("hour");
+
+function setTimeBlockColor(hour, timeBlock) {
+    var currentHour = moment().get("hour");
+    if (hour === currentHour) {
+        timeBlock.addClass("present");
+    } else if (hour < currentHour) {
+        timeBlock.addClass("past");
+    } else {
+        timeBlock.addClass("future");
+    }
+}
+
+function createRow(hourIndex) {
+    var container = $(".container");
+    var row = $("<div>").addClass("row");
+    var hour = $("<div>").addClass("hour col-md-2").text("" + hourIndex + "AM");
+    var timeBlock = $("<textArea>").addClass("time-block description col-md-8").attr("data-hour", hourIndex);
+    setTimeBlockColor(hourIndex, timeBlock);
+    var text;
+    if (text = localStorage.getItem(hourIndex)) {
+        timeBlock.text(text);
+    }
+    var saveButton = $("<div>").addClass("saveBtn col-md-2").attr("data-hour", hourIndex);
+    var saveImage = $("<img>").attr("src", "./images/icons8-save-48.png");
+    saveButton.append(saveImage);
+    row.append(hour).append(timeBlock).append(saveButton);
+    container.append(row);
+}
+
+//AM business hours
+for (var hourIndex = 9; hourIndex < 13; hourIndex++) {
+    createRow(hourIndex);
+}
+
+//PM business hours
+for (var hourIndex = 1; hourIndex < 6; hourIndex++) {
+    createRow(hourIndex);
+}
+
+$(".saveBtn").click(function () {
+    var dataHour = $(this).attr("data-hour");
+    var textArea = $("textArea[data-hour=" + dataHour + "]");
+
+    var text = textArea.val();
+    if (text) {
+        localStorage.setItem(dataHour, text);
+    }
+});
+
